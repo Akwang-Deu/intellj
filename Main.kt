640ea7095ceed7a0627e3aa66422bdc74e2e9157
Akwang-Deu <akwangdeu@gmail.com>
2024-02-11 12:59:26 +0300

    add file

---
+++ Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var fullname="Akwang Deu"
+    println(fullname)
+
+    var age= 21
+    println(age)
+
+    var phonenumber=701339743
+    println(phonenumber)
+
+    var weight= 56
+    println(weight)
+
+    var nationality="South Sudan"
+    println(nationality)
+
+    val citizen ="true"
+    println(citizen)
+
+    val noncitizen="false"
+    println(noncitizen)
+
+}
